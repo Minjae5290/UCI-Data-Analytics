9b01e570007c1a328c46cb95c160c97009369127
Minjae5290 <41414380+Minjae5290@users.noreply.github.com>
2018-08-19 12:09:57 -0700

    Update main.py

--- 03-Python_challenge/main.py
+++ 03-Python_challenge/main.py
@@ -48,7 +48,7 @@ output = (f"\nFinancial Analysis\n"
     f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
     f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
 
-file_to_output = os.path.join("output", "budget_data_analysis.txt")
+budget_data_analysis = os.path.join("output", "budget_data_analysis.xlsx")
 
 print(output)
 
@@ -61,97 +61,68 @@ with open(file_to_output, "w") as xlsx_file:
 
 --------------------------------------------------------------------------
 
-
-# Incorporated the csv module
 import os
 import csv
 
-# Files to load and output (Remember to change these)
-file_to_load = os.path.join("Resources", "election_data.csv")
-file_to_output = os.path.join("analysis", "election_analysis.txt")
+election_data = os.path.join("..","03-Python_challenge", "election_data.csv")
 
-# Total Vote Counter
 total_votes = 0
 
-# Candidate Options and Vote Counters
 candidate_options = []
 candidate_votes = {}
 
-# Winning Candidate and Winning Count Tracker
 winning_candidate = ""
 winning_count = 0
 
-# Read the csv and convert it into a list of dictionaries
-with open(file_to_load) as election_data:
-    reader = csv.reader(election_data)
+with open(election_data) as csvfile:
+    csvreader = csv.reader(csvfile)
 
-    # Read the header
-    header = next(reader)
+    header = next(csvreader)
 
-    # For each row...
-    for row in reader:
+    for row in csvreader:
 
-        # Run the loader animation
         print(". ", end=""),
 
-        # Add to the total vote count
         total_votes = total_votes + 1
 
-        # Extract the candidate name from each row
         candidate_name = row[2]
 
-        # If the candidate does not match any existing candidate...
-        # (In a way, our loop is "discovering" candidates as it goes)
         if candidate_name not in candidate_options:
 
-            # Add it to the list of candidates in the running
             candidate_options.append(candidate_name)
 
-            # And begin tracking that candidate's voter count
             candidate_votes[candidate_name] = 0
 
-        # Then add a vote to that candidate's count
         candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
 
-# Print the results and export the data to our text file
 with open(file_to_output, "w") as txt_file:
 
-    # Print the final vote count (to terminal)
-    election_results = (
-        f"\n\nElection Results\n"
+    election_results = (f"\n\nElection Results\n"
         f"-------------------------\n"
         f"Total Votes: {total_votes}\n"
         f"-------------------------\n")
     print(election_results, end="")
 
-    # Save the final vote count to the text file
     txt_file.write(election_results)
 
-    # Determine the winner by looping through the counts
     for candidate in candidate_votes:
 
-        # Retrieve vote count and percentage
         votes = candidate_votes.get(candidate)
         vote_percentage = float(votes) / float(total_votes) * 100
 
-        # Determine winning vote count and candidate
         if (votes > winning_count):
             winning_count = votes
             winning_candidate = candidate
 
-        # Print each candidate's voter count and percentage (to terminal)
         voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
         print(voter_output, end="")
 
-        # Save each candidate's voter count and percentage to text file
-        txt_file.write(voter_output)
+        xlsx_file.write(voter_output)
 
-    # Print the winning candidate (to terminal)
-    winning_candidate_summary = (
-        f"-------------------------\n"
+    winner_summary = (f"-------------------------\n"
         f"Winner: {winning_candidate}\n"
         f"-------------------------\n")
-    print(winning_candidate_summary)
+    print(winner_summary)
 
-    # Save the winning candidate's name to the text file
-    txt_file.write(winning_candidate_summary)
+    file_to_output = os.path.join("analysis", "election_analysis.xlsx")
+    xlsx_file.write(winner_summary)
